e7813a41710a5d788e8e416c507ca53dad3f6304
leon <leonvogt2001@gmail.com>
2023-08-05 14:55:22 +0200

    (add) add seperated memory areas

--- System/Core/Memory.cpp
+++ System/Core/Memory.cpp
@@ -1,61 +1,145 @@
 #include "Memory.h"
-#include <iostream>
 
 Memory::Memory(uint16_t *address) {
     SP = std::make_unique<uint16_t>(*address);
-    memory = std::vector<uint8_t>(MEMORY_SIZE, 0);
+
+    cartridge = std::make_unique<Cartridge>();
+
+    vram = std::vector<uint8_t>(VRAM_SIZE, 0);
+    externalRam = std::vector<uint8_t>(RAM_SIZE, 0);
+    wramBank0 = std::vector<uint8_t>(WRAM_BANK, 0);
+    wramBank1 = std::vector<uint8_t>(WRAM_BANK, 0);
+    echo = std::vector<uint8_t>(ECHO_RAM, 0);
+    oam = std::vector<uint8_t>(OAM_RAM, 0);
+    io = std::vector<uint8_t>(IO, 0);
+    hram = std::vector<uint8_t>(H_RAM, 0);
+
+    interrupt = 0;
 }
 
-Memory::~Memory() {
-
-}
+Memory::~Memory() = default;
 
 bool inline inRange(uint16_t address, uint16_t lowerBound, uint16_t upperBound) {
     return address >= lowerBound && address <= upperBound;
 }
 
 uint8_t Memory::readByte(uint16_t address) const {
-    if (inRange(address, romBankStart, romBankEnd)) {
-        //TODO
-    } else if (inRange(address, switchRomBankStart, switchRomBankEnd)) {
-        //TODO
-    } else if (inRange(address, VRAMstart, VRAMend)) {
-        //TODO
-    } else if (inRange(address, internalRAMstart, internalRAMend)) {
-
-    } else if (inRange(address, spriteAttEnd, spriteAttEnd)) {
-        //TODO
-    } else if (inRange(address, inputOutputStart, inputOutputEnd)) {
-        //TODO
-    } else if (inRange(address, highRAMstart, highRAMend)) {
-        //TODO
-    } else if (address == interruptRegister) {
-        //TODO
-    }
-    return memory[address];
+    if (inRange(address, 0x0000, 0x7FFF)) return cartridge->readByte(address);
+    else if (inRange(address, 0x8000, 0x9FFF)) return vram[address - 0x8000];
+    else if (inRange(address, 0xA000, 0xBFFF)) return externalRam[address - 0xA000];
+    else if (inRange(address, 0xC000, 0xCFFF)) return wramBank1[address - 0xC000];
+    else if (inRange(address, 0xD000, 0xDFFF)) return wramBank1[address - 0xD000];
+    else if (inRange(address, 0xE000, 0xFDFF)) return echo[address - 0xE000];
+    else if (inRange(address, 0xFE00, 0xFE9F)) return oam[address - 0xFE00];
+    else if (inRange(address, 0xFF00, 0xFF7F)) return io[address - 0xFF00];
+    else if (inRange(address, 0xFF80, 0xFFFE)) return hram[address - 0xFF80];
+    else if (address == 0xFFFF) return interrupt;
+    else std::cout << "Invalid Address" << std::endl;
 }
 
 void Memory::writeByte(uint16_t address, uint8_t value) {
-    memory[address] = value;
+    if (inRange(address, 0x0000, 0x7FFF)) {}//cartridge->readByte(address); //= value;
+    else if (inRange(address, 0x8000, 0x9FFF)) {
+        std::cout << "Accessing VRAM" << std::endl;
+        vram[address - 0x8000] = value;
+    }
+    else if (inRange(address, 0xA000, 0xBFFF)) externalRam[address - 0xA000] = value;
+    else if (inRange(address, 0xC000, 0xCFFF)) wramBank1[address - 0xC000] = value;
+    else if (inRange(address, 0xD000, 0xDFFF)) wramBank1[address - 0xD000] = value;
+    else if (inRange(address, 0xE000, 0xFDFF)) echo[address - 0xE000] = value;
+    else if (inRange(address, 0xFE00, 0xFE9F)) oam[address - 0xFE00] = value;
+    else if (inRange(address, 0xFF00, 0xFF7F)) io[address - 0xFF00] = value;
+    else if (inRange(address, 0xFF80, 0xFFFE)) hram[address - 0xFF80] = value;
+    else if (address == 0xFFFF) interrupt = value;
+    else std::cout << "Invalid Address" << std::endl;
 }
 
 uint16_t Memory::readWord(uint16_t address) const {
-    uint8_t lowByte = memory[address];
-    uint8_t highByte = memory[address + 1];
+    uint8_t lowByte = 0;
+    uint8_t highByte = 0;
+
+    if (inRange(address, 0x0000, 0x7FFF)) {
+        lowByte = cartridge->readByte(address);
+        highByte = cartridge->readByte(address + 1);
+    } else if (inRange(address, 0x8000, 0x9FFF)) {
+        address -= 0x8000;
+        lowByte = vram[(address)];
+        highByte = vram[(address) + 1];
+    } else if (inRange(address, 0xA000, 0xBFFF)) {
+        address -= 0xA000;
+        lowByte = externalRam[(address)];
+        highByte = externalRam[(address) + 1];
+    } else if (inRange(address, 0xC000, 0xCFFF)) {
+        address -= 0xC000;
+        lowByte = wramBank0[(address)];
+        highByte = wramBank0[(address) + 1];
+    } else if (inRange(address, 0xD000, 0xDFFF)) {
+        address -= 0xD000;
+        lowByte = wramBank1[(address)];
+        highByte = wramBank1[(address) + 1];
+    } else if (inRange(address, 0xE000, 0xFDFF)) {
+        address -= 0xE000;
+        lowByte = echo[address];
+        highByte = echo[(address) + 1];
+    } else if (inRange(address, 0xFE00, 0xFE9F)) {
+        address -= 0xFE00;
+        lowByte = oam[address];
+        highByte = oam[(address) + 1];
+    } else if (inRange(address, 0xFF00, 0xFF7F)) {
+        address -= 0xFF00;
+        lowByte = io[address];
+        highByte = io[(address) + 1];
+    } else if (inRange(address, 0xFF80, 0xFFFE)) {
+        address -= 0xFF80;
+        lowByte = hram[address];
+        highByte = hram[(address) + 1];
+    } else if (address == 0xFFFF) return interrupt;
+    else std::cout << "Invalid Address" << std::endl;
     return static_cast<uint16_t>((highByte << 8) | lowByte);
 }
 
 void Memory::writeWord(uint16_t address, uint16_t value) {
     uint8_t lowByte = value & 0xFF;
     uint8_t highByte = value >> 8;
-    memory[address] = lowByte;
-    memory[address + 1] = highByte;
+
+    if (inRange(address, 0x0000, 0x7FFF)) {
+    } else if (inRange(address, 0x8000, 0x9FFF)) {
+        address -= 0x8000;
+        vram[address] = lowByte;
+        vram[address + 1] = highByte;
+    } else if (inRange(address, 0xA000, 0xBFFF)) {
+        address -= 0xA000;
+        externalRam[address] = lowByte;
+        externalRam[address + 1] = highByte;
+    } else if (inRange(address, 0xC000, 0xCFFF)) {
+        address -= 0xC000;
+        wramBank0[address] = lowByte;
+        wramBank0[address + 1] = highByte;
+    } else if (inRange(address, 0xD000, 0xDFFF)) {
+        address -= 0xD000;
+        wramBank1[address] = lowByte;
+        wramBank1[address + 1] = highByte;
+    } else if (inRange(address, 0xE000, 0xFDFF)) {
+        address -= 0xE000;
+        echo[address] = lowByte;
+        echo[address + 1] = highByte;
+    } else if (inRange(address, 0xFE00, 0xFE9F)) {
+        address -= 0xFE00;
+        oam[address] = lowByte;
+        oam[(address) + 1] = highByte;
+    } else if (inRange(address, 0xFF00, 0xFF7F)) {
+        address -= 0xFF00;
+        io[address] = lowByte;
+        io[(address) + 1] = highByte;
+    } else if (inRange(address, 0xFF80, 0xFFFE)) {
+        address -= 0xFF80;
+        hram[address - 0xFF80] = lowByte;
+        hram[(address) + 1] = highByte;
+    } else std::cout << "Invalid Address" << std::endl;
 }
 
-void Memory::loadROM(const uint8_t *romData, size_t romSize) {
-    if (romData && romSize <= (MEMORY_SIZE - 0x8000)) {
-        memcpy(&memory[0x0000], romData, romSize);
-    }
+std::vector<uint8_t> *Memory::getVram() {
+    return &vram;
 }
 
 uint8_t Memory::popByte() {
@@ -80,44 +164,44 @@ void Memory::pushWord(uint16_t value) {
     (*SP) -= 2;
 }
 
-uint8_t &Memory::operator[](uint16_t address) {
-    return memory[address];
+void Memory::initMemory() {
+
+    for(size_t i = 0; i < VRAM_SIZE; i++){
+        vram[i] = cartridge->readByte(i);
+    }
+
+    /* memory[0xFF05] = 0x00;
+     memory[0xFF06] = 0x00;
+     memory[0xFF07] = 0x00;
+     memory[0xFF10] = 0x80;
+     memory[0xFF11] = 0xBF;
+     memory[0xFF12] = 0xF3;
+     memory[0xFF14] = 0xBF;
+     memory[0xFF16] = 0x3F;
+     memory[0xFF17] = 0x00;
+     memory[0xFF19] = 0xBF;
+     memory[0xFF1A] = 0x7F;
+     memory[0xFF1B] = 0xFF;
+     memory[0xFF1C] = 0x9F;
+     memory[0xFF1E] = 0xBF;
+     memory[0xFF20] = 0xFF;
+     memory[0xFF21] = 0x00;
+     memory[0xFF22] = 0x00;
+     memory[0xFF23] = 0xBF;
+     memory[0xFF24] = 0x77;
+     memory[0xFF25] = 0xF3;
+     memory[0xFF26] = 0xF1;
+     memory[0xFF40] = 0x91;
+     memory[0xFF42] = 0x00;
+     memory[0xFF43] = 0x00;
+     memory[0xFF45] = 0x00;
+     memory[0xFF47] = 0xFC;
+     memory[0xFF48] = 0xFF;
+     memory[0xFF49] = 0xFF;
+     memory[0xFF4A] = 0x00;
+     memory[0xFF4B] = 0x00;
+     memory[0xFFFF] = 0x00;
+     */
 }
 
-const uint8_t &Memory::operator[](uint16_t address) const {
-    return memory[address];
-}
 
-void Memory::init() {
-    memory[0xFF05] = 0x00;
-    memory[0xFF06] = 0x00;
-    memory[0xFF07] = 0x00;
-    memory[0xFF10] = 0x80;
-    memory[0xFF11] = 0xBF;
-    memory[0xFF12] = 0xF3;
-    memory[0xFF14] = 0xBF;
-    memory[0xFF16] = 0x3F;
-    memory[0xFF17] = 0x00;
-    memory[0xFF19] = 0xBF;
-    memory[0xFF1A] = 0x7F;
-    memory[0xFF1B] = 0xFF;
-    memory[0xFF1C] = 0x9F;
-    memory[0xFF1E] = 0xBF;
-    memory[0xFF20] = 0xFF;
-    memory[0xFF21] = 0x00;
-    memory[0xFF22] = 0x00;
-    memory[0xFF23] = 0xBF;
-    memory[0xFF24] = 0x77;
-    memory[0xFF25] = 0xF3;
-    memory[0xFF26] = 0xF1;
-    memory[0xFF40] = 0x91;
-    memory[0xFF42] = 0x00;
-    memory[0xFF43] = 0x00;
-    memory[0xFF45] = 0x00;
-    memory[0xFF47] = 0xFC;
-    memory[0xFF48] = 0xFF;
-    memory[0xFF49] = 0xFF;
-    memory[0xFF4A] = 0x00;
-    memory[0xFF4B] = 0x00;
-    memory[0xFFFF] = 0x00;
-}
--- System/Core/Memory.h
+++ System/Core/Memory.h
@@ -1,58 +1,61 @@
 #include <cstdint>
 #include <vector>
 #include <memory>
+#include <iostream>
 
-constexpr uint16_t MEMORY_SIZE = 0xFFFF;
+#include "Cartridge.h"
 
 class Memory
 {
 private:
-    std::vector<uint8_t> memory;
+    //Interrupt Flags (IF) Register
+    const uint8_t IF_VBLANK = 0x01; // Vertical Blank Interrupt
+    const uint8_t IF_LCDSTAT = 0x02; // LCD Status Interrupt
+    const uint8_t IF_TIMER = 0x04; // Timer Interrupt
+    const uint8_t IF_SERIAL = 0x08; // Serial Transfer Interrupt
+    const uint8_t IF_JOYPAD = 0x10;	// Joypad Interrupt
+
+    //const int ROM_SIZE = 0x8000;
+    const int RAM_SIZE = 0x2000;
+    const int VRAM_SIZE = 0x2000;
+    const int WRAM_BANK = 0x1000;
+    const int ECHO_RAM = 0x1E00;
+    const int OAM_RAM = 0x00A0;
+    const int IO = 0x0080;
+    const int H_RAM = 0x007F;
+
+    std::unique_ptr<Cartridge> cartridge;
+
+    std::vector<uint8_t> vram;
+    std::vector<uint8_t> externalRam;
+    std::vector<uint8_t> wramBank0;
+    std::vector<uint8_t> wramBank1;
+    std::vector<uint8_t> echo;
+    std::vector<uint8_t> oam;
+    std::vector<uint8_t> ram;
+    std::vector<uint8_t> hram;
+    std::vector<uint8_t> io;
+
+    uint8_t interrupt;
+
     std::unique_ptr<uint16_t> SP;
 
-    static constexpr uint16_t romBankStart = 0x0000;
-    static constexpr uint16_t romBankEnd = 0x3FFF;
-    static constexpr uint16_t switchRomBankStart = 0x4000;
-    static constexpr uint16_t switchRomBankEnd = 0x7FFF;
-    static constexpr uint16_t VRAMstart = 0x8000;
-    static constexpr uint16_t VRAMend = 0x9FFF;
-    static constexpr uint16_t switchVRAMstart = 0xA000;
-    static constexpr uint16_t switchVRAMend = 0xBFFF;
-    static constexpr uint16_t internalRAMstart = 0xC000;
-    static constexpr uint16_t internalRAMend = 0xDFFF;
-    static constexpr uint16_t unusableStart = 0xE000;
-    static constexpr uint16_t unusableEnd = 0xFDFF;
-    static constexpr uint16_t spriteAttStart = 0xFE00;
-    static constexpr uint16_t spriteAttEnd = 0xFE9F;
-    static constexpr uint16_t unusable2Start = 0xFEA0;
-    static constexpr uint16_t unusable2End = 0xFEFF;
-    static constexpr uint16_t inputOutputStart = 0xFF00;
-    static constexpr uint16_t inputOutputEnd = 0xFF4B;
-    static constexpr uint16_t unusable3Start = 0xFF4C;
-    static constexpr uint16_t unusable3End = 0xFF7F;
-    static constexpr uint16_t highRAMstart = 0xFF80;
-    static constexpr uint16_t highRAMend = 0xFFFE;
-    static constexpr uint16_t interruptRegister = 0xFFFF;
-
 public:
-    Memory(uint16_t* address);
+    explicit Memory(uint16_t* address);
     ~Memory();
 
-    void init();
+    std::vector<uint8_t>* getVram();
 
-    uint8_t readByte(uint16_t address) const;
+    void initMemory();
+
+    [[nodiscard]] uint8_t readByte(uint16_t address) const;
     void writeByte(uint16_t address, uint8_t value);
-    uint16_t readWord(uint16_t address) const;
+    [[nodiscard]] uint16_t readWord(uint16_t address) const;
     void writeWord(uint16_t address, uint16_t value);
 
     uint8_t popByte();
     void pushByte(uint8_t value);
     uint16_t popWord();
     void pushWord(uint16_t value);
-
-    void loadROM(const uint8_t* romData, size_t romSize);
-
-    uint8_t& operator[](uint16_t address);
-    const uint8_t& operator[](uint16_t address) const;
 };
 
