fdca826198fcdfad125d19913fe75ffe8116a3d2
leon <leonvogt2001@gmail.com>
2023-08-07 02:28:54 +0200

    restructure

--- System/Emulator/Emulator.cpp
+++ System/Emulator/Emulator.cpp
@@ -1,97 +1,17 @@
 //
-// Created by leonv on 31/07/2023.
+// Created by leon on 8/6/23.
 //
 
 #include "Emulator.h"
 
 Emulator::Emulator() {
     cpu = new CPU();
-    ppu = new PPU((cpu->memory->getVram()));
-    ppu->loadVRAM();
-    ppu->debugView();
-    //this->start();
 }
 
 Emulator::~Emulator() {
-    delete ppu;
     delete cpu;
 }
 
 void Emulator::start() {
-    cpu->initCPU();
-
-    while (true) {
-        int currentCycle = 0;
-
-        while (currentCycle < CYCLES_PER_FRAME) {
-            std::cout << static_cast<int>(currentCycle) << std::endl;
-            cpu->fetchOpCode();
-            currentCycle += cpu->cycle;
-            cpu->handleInterrupts();
-         //   cpu->printStatus();
-            //ppu->debug();
-        }
-    }
-}
-
-void Emulator::debug() {
-    uint8_t cycle = 0;
-    //cpu->fetchOpCode(cycle);
-
-    cyclesRemaining -= cycle;
-
-    uint16_t address = 0x0100;
-    uint16_t data = 0xC005;
-    uint16_t readData = 0xC010;
-
-    // Write data into accumulator
-    cpu->writeByte(address++, 0xFA);
-    cpu->writeByte(data, static_cast<uint8_t>(210));
-    cpu->writeWord(address, data);
-    address += 2;
-
-    // Add data with accumulator
-    cpu->writeByte(address++, 0xDE);
-    cpu->writeByte(address++, static_cast<uint8_t>(110));
-
-    // Store result in memory address 0x0001
-    cpu->writeByte(address++, 0xEA);
-    cpu->writeWord(address, readData);
-    address += 2;
-
-    std::cout << "Cycle: " << static_cast<int>(cycle) << "\t Remaining Cycles: " << static_cast<int>(cyclesRemaining) << std::endl;
-
-    // Execute instructions
-    cpu->fetchOpCode();
-    cpu->printStatus();
-
-    cyclesRemaining -= cycle;
-    std::cout << "Cycle: " << static_cast<int>(cycle) << "\t Remaining Cycles: " << static_cast<int>(cyclesRemaining) << std::endl;
-
-    cpu->fetchOpCode();
-    cpu->printStatus();
-
-    cyclesRemaining -= cycle;
-    std::cout << "Cycle: " << static_cast<int>(cycle) << "\t Remaining Cycles: " << static_cast<int>(cyclesRemaining) << std::endl;
-
-    cpu->fetchOpCode();
-    cpu->printStatus();
-
-    cyclesRemaining -= cycle;
-    std::cout << "Cycle: " << static_cast<int>(cycle) << "\t Remaining Cycles: " << static_cast<int>(cyclesRemaining) << std::endl;
-
-    int value = static_cast<int>(cpu->readByte(readData));
-
-    std::cout << "Result: " << value << std::endl;
-}
-
-void Emulator::initEmulator() {
 
 }
-
-void Emulator::restart() {
-}
-
-void Emulator::poweroff() {
-}
-
--- System/Emulator/Emulator.h
+++ System/Emulator/Emulator.h
@@ -1,40 +1,21 @@
 //
-// Created by leonv on 31/07/2023.
+// Created by leon on 8/6/23.
 //
 
 #ifndef GAMEBOYPLUS_EMULATOR_H
 #define GAMEBOYPLUS_EMULATOR_H
 
-#include "../Core/CPU.h"
-#include "../Core/PPU.h"
+#include "../Core/CPU/CPU.h"
 
-#include <iostream>
-
-class Emulator
-{
+class Emulator {
 private:
-    const uint8_t framerate = 60;
-    const uint32_t CLOCK_SPEED = 4194304;
-    const uint32_t CYCLES_PER_FRAME = CLOCK_SPEED / framerate;
-    int32_t cyclesRemaining{};
-
     CPU* cpu;
-    PPU* ppu;
 
 public:
     Emulator();
     ~Emulator();
 
-    void debug();
-
-    void initEmulator();
     void start();
-    void restart();
-    void poweroff();
-
-    void saveGame();
-    void loadGame();
-
 };
 
 
