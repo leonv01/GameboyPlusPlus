67c64609c4a90bf542b420f1844e48c7fcd9a1f9
leon <leonvogt2001@gmail.com>
2023-08-07 02:28:20 +0200

    (add) information printout and cartridge address reading

--- System/Core/Cartridge.cpp
+++ System/Core/Cartridge.cpp
@@ -6,14 +6,12 @@
 
 Cartridge::Cartridge() {
     rom = new char[ROM_SIZE];
-    title = new char[TITLE_LENGTH];
-
+    title = new char[0xF];
     loadROM();
 }
 
 Cartridge::~Cartridge() {
     delete rom;
-    delete title;
 }
 
 uint16_t Cartridge::readWord(uint16_t address) const {
@@ -35,24 +33,158 @@ void Cartridge::loadROM() {
         romFile.read(rom, ROM_SIZE);
         romFile.close();
 
-        for(size_t i = 0x134; i <= 0x0143; i++) {
-            title [i - 0x134] = rom[i];
-            std::cout << title[i - 0x134] << " ";
-        }
-        std:: cout << std::endl;
-
         licenseCode = rom[0x0145];
-
-        romSize = rom[0x0147];
+        sgbFlag = rom[0x0146];
+        cartridgeType = rom[0x0147];
+        romSize = rom[0x0148];
+        ramSize = rom[0x0149];
+        newLicensee = static_cast<uint16_t>((rom[0x0144] << 8) | rom[0x0145]);
+        destinationCode = rom[0x014A];
+        oldLicensee = rom[0x014B];
+        maskRom = rom[0x014C];
+        headerChecksum = rom[0x014D];
 
         std::cout << static_cast<unsigned int>(licenseCode) << std::endl;
         std::cout << static_cast<unsigned int>(romSize) << std::endl;
-
-
-
-
+        std::cout
+        << "Title: " << getTitle() << std::endl
+        << "New Licensee: " << "0x" << std::hex << static_cast<int>(newLicensee) << " " << static_cast<int>(newLicensee) << std::endl
+        << "Old Licensee: " << "0x" << std::hex << static_cast<int>(oldLicensee) << " " << static_cast<int>(oldLicensee) << std::endl
+        << "Cartridge Type: " << getCartridgeType() << std::endl
+        << "ROM Size: " << "0x" << std::hex << static_cast<int>(romSize) << " " << static_cast<int>(romSize) << " | " << getRomSize() << std::endl
+        << "RAM Size: " << "0x" << std::hex << static_cast<int>(ramSize) << " " << static_cast<int>(ramSize) << " | " << getRamSize() << std::endl
+        << "Destination Code: " << std::hex << static_cast<int>(destinationCode) << std::endl
+        << "Mask ROM Version Number: " << std::hex << static_cast<int>(maskRom) << std::endl
+        << "Header Checksum: " << std::hex << static_cast<int>(headerChecksum) << " | " << std::hex << static_cast<int>(getHeaderChecksum()) << std::endl
+        ;
         //for(size_t i = 0; i < ROM_SIZE; i++) std::cout << static_cast<unsigned int>(rom[i]) << " ";
-
     }
+}
+
+std::string Cartridge::getTitle() {
+    for(size_t i = 0x134; i <= 0x0143; i++) {
+        title [i - 0x134] = rom[i];
+    }
+    return title;
+}
+
+uint8_t Cartridge::getHeaderChecksum(){
+    uint8_t checkSum = 0;
+    for(uint16_t address = 0x0134; address <= 0x014C; address++){
+        checkSum = checkSum - rom[address] - 1;
+    }
+    return checkSum;
+}
+
+std::string Cartridge::getRamSize() const {
+    std::string output;
+    switch (romSize) {
+        case 0x00:
+            output = "0\tNo RAM";
+            break;
+        case 0x01:
+            output = "-\tUnused";
+            break;
+        case 0x02:
+            output = "8 KiB\t1 Bank";
+            break;
+        case 0x03:
+            output = "32 KiB\t4 Banks of 8 KiB each";
+            break;
+        case 0x04:
+            output = "128 KiB\t16 Banks of 8 KiB each";
+            break;
+        case 0x05:
+            output = "64 KiB\t8 Banks of 8 KiB each";
+            break;
+        default:
+            output = "ERROR";
+            break;
+    }
+    return output;
+}
+std::string Cartridge::getRomSize() const {
+    std::string output;
+    switch(romSize){
+        case 0x00: output = "32 KiB\t2 ROM Banks (no banking)"; break;
+        case 0x01: output = "64 KiB\t4 ROM Banks"; break;
+        case 0x02: output = "128 KiB\t8 ROM Banks"; break;
+        case 0x03: output = "256 KiB\t16 ROM Banks"; break;
+        case 0x04: output = "512 KiB\t32 ROM Banks"; break;
+        case 0x05: output = "1 MiB\t64 ROM Banks"; break;
+        case 0x06: output = "2 MiB\t128 ROM Banks"; break;
+        case 0x07: output = "4 MiB\t256 ROM Banks"; break;
+        case 0x08: output = "8 MiB\t512 ROM Banks"; break;
+        case 0x52: output = "1.1 MiB\t72 ROM Banks"; break;
+        case 0x53: output = "1.2 MiB\t80 ROM Banks"; break;
+        case 0x54: output = "1.5 MiB\t96 ROM Banks"; break;
+        default: output = "ERROR"; break;
+    }
+    return output;
+}
+
+std::string Cartridge::getCartridgeType() const {
+    std::string output;
+    switch(cartridgeType){
+        case 0x00:
+            output = "ROM ONLY"; break;
+        case 0x01:
+            output = "MBC1"; break;
+        case 0x02:
+            output = "MBC1+RAM"; break;
+        case 0x03:
+            output = "MBC1+RAM+BATTERY"; break;
+        case 0x05:
+            output = "MBC2"; break;
+        case 0x06:
+            output = "MBC2+BATTERY"; break;
+        case 0x08:
+            output = "ROM+RAM"; break;
+        case 0x09:
+            output = "ROM+RAM+BATTERY"; break;
+        case 0x0B:
+            output = "MMM01"; break;
+        case 0x0C:
+            output = "MMM01+RAM"; break;
+        case 0x0D:
+            output = "MMM01+RAM+BATTERY"; break;
+        case 0x0F:
+            output = "MBC3+TIMER+BATTERY"; break;
+        case 0x10:
+            output = "MBC3+TIMER+RAM+BATTERY"; break;
+        case 0x11:
+            output = "MBC3"; break;
+        case 0x12:
+            output = "MBC3+RAM"; break;
+        case 0x13:
+            output = "MBC3+RAM+BATTERY"; break;
+        case 0x19:
+            output = "MBC5"; break;
+        case 0x1A:
+            output = "MBC5+RAM"; break;
+        case 0x1B:
+            output = "MBC5+RAM+BATTERY"; break;
+        case 0x1C:
+            output = "MBC5+RUMBLE"; break;
+        case 0x1D:
+            output = "MBC5+RUMBLE+RAM"; break;
+        case 0x1E:
+            output = "MBC5+RUMBLE+RAM+BATTERY"; break;
+        case 0x20:
+            output = "MBC6"; break;
+        case 0x22:
+            output = "MBC7+SENSOR+RUMBLE+RAM+BATTERY"; break;
+        case 0xFC:
+            output = "POCKET CAMERA"; break;
+        case 0xFD:
+            output = "BANDAI TAMA5"; break;
+        case 0xFE:
+            output = "HuC3"; break;
+        case 0xFF:
+            output = "HuC1+RAM+BATTERY"; break;
+        default:
+            output = "ERROR"; break;
+    }
+    return output;
 }
 
--- System/Core/Cartridge.h
+++ System/Core/Cartridge.h
@@ -18,11 +18,24 @@ private:
     const int ROM_SIZE = 0x2000;
     const int TITLE_LENGTH = 0xF;
     char licenseCode{};
-    char romSize{};
     char* rom;
-    char* title;
+    int RAM_SIZE{};
 
-    std::string path = "/home/leon/Documents/GameboyPlusPlus/GameROM/SuperMario.gb";
+    char* title;          //0x0134 - 0x0143
+    //   uint16_t manufacturer;   //0x0144- 0x0145
+    uint8_t cgbFlag{};        //0x143
+    uint16_t newLicensee{};    //0x0144 - 0x0145
+    uint8_t sgbFlag{};        //0x0146
+    uint8_t cartridgeType{};  //0x0147
+    uint8_t romSize{};        //0x0148
+    uint8_t ramSize{};        //0x0149
+    uint8_t destinationCode{};//0x014A
+    uint8_t oldLicensee{};    //0x014B
+    uint8_t maskRom{};        //0x014C
+    uint8_t headerChecksum{}; //0x014D
+    uint8_t globalChecksum{}; //0x014E - 0x014F
+
+    std::string path = "/home/leon/Documents/GameboyPlusPlus/GameROM/Tetris.gb";
 public:
 
     Cartridge();
@@ -32,6 +45,13 @@ public:
 
     [[nodiscard]] uint8_t readByte(uint16_t address) const;
     [[nodiscard]] uint16_t readWord(uint16_t address) const;
+
+
+    std::string getTitle();
+    [[nodiscard]] std::string getCartridgeType() const;
+    [[nodiscard]] std::string getRomSize() const;
+    [[nodiscard]] std::string getRamSize() const;
+    uint8_t getHeaderChecksum();
 };
 
 
