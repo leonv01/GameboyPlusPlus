709700e5df90092065652ea57e544e4d863e1b44
leon <leonvogt2001@gmail.com>
2023-08-12 11:59:39 +0200

    (add) run and timing definitions

--- System/Emulator/Emulator.cpp
+++ System/Emulator/Emulator.cpp
@@ -6,12 +6,36 @@
 
 Emulator::Emulator() {
     cpu = new CPU();
+    ppu = new PPU();
+    timer = nullptr;
 }
 
 Emulator::~Emulator() {
+    delete timer;
+    delete ppu;
     delete cpu;
+
 }
 
 void Emulator::start() {
+    timer = new clock_t;
 
+    while(true){
+        int currentCycle{};
+        cpu->handleInterrupt();
+
+        while(currentCycle < cyclesPerFrame){
+            uint8_t opcode = cpu->fetchOpCode();
+            std::cout << std::hex << static_cast<int>(opcode) << std::endl;
+            cpu->parseOpCode(opcode);
+            currentCycle += cpu->cycle;
+
+            ppu->updatePPU();
+            cpu->updateTimer(currentCycle);
+
+
+
+            cpu->cycle = 0;
+        }
+    }
 }
--- System/Emulator/Emulator.h
+++ System/Emulator/Emulator.h
@@ -1,15 +1,21 @@
-//
-// Created by leon on 8/6/23.
-//
-
 #ifndef GAMEBOYPLUS_EMULATOR_H
 #define GAMEBOYPLUS_EMULATOR_H
 
 #include "../Core/CPU/CPU.h"
+#include "../Core/PPU/PPU.h"
+
+#include <ctime>
 
 class Emulator {
 private:
+    const int FPS = 60;
+    const int CLOCKSPEED = 4194304;
+
+    const float cyclesPerFrame = static_cast<float>(CLOCKSPEED) / static_cast<float>(FPS);
+
     CPU* cpu;
+    PPU* ppu;
+    clock_t* timer;
 
 public:
     Emulator();
