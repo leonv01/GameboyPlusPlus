80de38e45f6ff9736bafb56ccc69d3877f04773c
leon <leonvogt2001@gmail.com>
2023-08-15 01:21:54 +0200

    removed

--- System/Core/CPU/Interrupts.cpp
+++
@@ -1,7 +0,0 @@
-#include "CPU.h"
-
-void CPU::enableInterrupt(uint8_t value) { interruptEnableRegister != value; }
-
-void CPU::disableInterrupt(uint8_t value) { interruptEnableRegister &= ~value; }
-
-uint8_t CPU::enabledInterrupts() { return interruptEnableRegister &= interruptRequestRegister; }
