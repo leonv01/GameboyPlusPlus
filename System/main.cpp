626c818ae71249b80a03a443d098a00178c5459f
leon <leonvogt2001@gmail.com>
2023-08-15 01:22:01 +0200

    restructure

--- System/main.cpp
+++ System/main.cpp
@@ -3,9 +3,9 @@
 #include "Emulator/Emulator.h"
 
 int main(){
-
     Emulator emulator;
-
+    emulator.loadROM("/home/leon/Documents/GameboyPlusPlus/GameROM/MortalKombat.gb");
+    emulator.start();
     return 0;
 
 }
